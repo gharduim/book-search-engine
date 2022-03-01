61c5648a1b991455ddae6af4a553e1295cf81981
Gustavo <gharduim@gmail.com>
2022-03-01 21:50:17 +1030

    fix login and signup

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  query {
+    me {
+        username
+        email
+        savedBooks{
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+        bookCount
+    }
+}
+`;
